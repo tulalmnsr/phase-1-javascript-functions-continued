d97bbee775ea33c4b1b923388b81e3cb22b56a9d
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-11-28 13:41:32 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+// Your code here
+function saturdayFun(arg1 = 'roller-skate'){
+    return `This Saturday, I want to ${arg1}!`
+   }
+   
+   function mondayWork(arg = 'go to the office'){
+    return `This Monday, I will ${arg}.`
+   }
+   
+   function wrapAdjective(string = '*'){ 
+   return function (arg = 'special') {
+    return `You are ${string}${arg}${string}!`
+    }
+   }
+   
+   const Calculator = {
+    add: function(a,b){
+   return a+b
+   }, 
+   subtract: function(a,b){
+   return a-b
+   },
+    multiply: function(a,b){
+   return a*b
+   },
+    divide: function(a,b){
+   return a/b
+   }
+   }
+   
+   function actionApplyer(int, array){
+    for (let i = 0; i < array.length; i++ ){
+    int = array[i](int)
+    } 
+   return int
+   }
